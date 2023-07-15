1d704a8e0d52bd25997f33e8971ca840a61b2797
DanielHonrales <danielhonrales@gmail.com>
2023-07-15 18:10:17 -0500

    Replaced firebase with local data

---
+++ app/equipment_verification.py
@@ -0,0 +1,17 @@
+import json
+import keyboard
+import asyncio
+import time
+from audioTranscription import *
+from common.constants import *
+
+def find_equipment_errors(equipment_data, equipment_errors):
+    while True:
+        start_audio_recog(equipment_data)
+        
+        for equipment in equipment_data:
+            if (equipment[KEY_EQUIPMENT_REQUESTED] != equipment[KEY_EQUIPMENT_PRESENT] and
+                not equipment[KEY_EQUIPMENT_LOCKED]):
+                print(f"Found error with {equipment}")
+                equipment_errors += f"{equipment}-{time.time()}"
+                equipment[KEY_EQUIPMENT_LOCKED] = True
